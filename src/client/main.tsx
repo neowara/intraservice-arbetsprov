179fbce84a5f66c8d97aef527def15e2fd5fa5e5
Christian Toledo <mugidota@hotmail.com>
2025-06-02 13:43:43 +0200

    chore: integrate CampRegistrationForm into main app

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
+import CampRegistrationForm from "./components/CampRegistrationForm";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <></>
+    <CampRegistrationForm />
   </React.StrictMode>
 );
